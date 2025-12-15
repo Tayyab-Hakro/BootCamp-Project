67023f15d130ca2558821771a4dec56f766e6092
Tayyab Hakro <tayyabhakro2k21@gmail.com>
2025-12-15 03:39:19 +0000

    models is updated

---
+++ pratice/src/ReportModal.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function ReportModal() {
+    export const Handlebtn  =() =>{
+    setIsOpenBtn(!isOpenBtn)
+    }
+const [isOpenData , SetIsOpenDATA] = useState(false)
+
+
+  return (
+    <div className='w-full min-w-2xl bg-gray-500 '>
+    </div>
+  )
+}
+
+export default ReportModal
